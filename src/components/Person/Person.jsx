2444205cc1d2e75de1397f0190c4f52be21bce62
annbisha <annskaylove@gmail.com>
2026-05-07 17:16:03 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,17 @@
-/* eslint-disable indent */
-
 export function Person({ person }) {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partnerMessage = isMarried
+    ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+    : 'I am not married';
+
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
 
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {age && <p className="Person__age">I am {age}</p>}
 
-      <p className="Person__partner">
-        {person.isMarried
-          ? `${person.partnerName} is my ${
-              person.sex === 'm' ? 'wife' : 'husband'
-            }`
-          : 'I am not married'}
-      </p>
+      <p className="Person__partner">{partnerMessage}</p>
     </section>
   );
 }
